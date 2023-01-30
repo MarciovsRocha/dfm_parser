2e69d804a0ab35c3a5708a208e764f7b01b92d40
Patrick E. M. Karvat <patrick.karvat@mps.com.br>
2023-01-30 11:58:04 -0300

    Pare de ser burro
    Assim que faz

--- main.py
+++ main.py
@@ -13,9 +13,10 @@ from utils import PROJ_PATH, CriarEstruturaLocal
 # CONSTANTS
 # Delpgi Form File path
 # file = PROJ_PATH+'\CPlanoInativo.dfm'
-comom_path = 'C:\projects\HomePar\Fontes\Comum\Classes\\'
+comom_path = ''
 
-classe = str(input('Exemplo: CPlanoinativo.dfm\nDigite o nome do arquivo DFM sua classe:\n'))
+# classe = str(input('Exemplo: CPlanoinativo.dfm\nDigite o nome do arquivo DFM sua classe:\n'))
+classe = 'C:\Projetos\HomePar\master\Fontes\Comum\Classes\CplanoInativo.dfm'
 
 file: str
 
@@ -32,7 +33,7 @@ parser.parse()
 lista_atributos = parser.get_lista_atributos()
 lista_fields = parser.get_lista_fields()
 estrutura_local = CriarEstruturaLocal(lista_atributos=lista_atributos, lista_fields=lista_fields)
-arquivo_exportado = f'estrutura_local_{classe}.txt'
+arquivo_exportado = 'estrutura_local.txt'
 with open(arquivo_exportado, 'w') as fwrite:
     fwrite.write(estrutura_local)
 input(f'Arquivo exportado para: {arquivo_exportado}\nAperte [Enter] para finalizar.')
